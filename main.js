6e4e8f27a11217e17a0cf057564b2609f61c614f
pastak <pasta0915@gmail.com>
2016-01-16 01:53:07 +0900

    ソーシャルボタン追加

--- main.js
+++ main.js
@@ -2,13 +2,14 @@
 require('whatwg-fetch')
 const React = require('react')
 const ReactDOM = require('react-dom')
+const shuffle = require('knuth-shuffle').knuthShuffle
 
 fetch('/images.json')
   .then((res) => res.json())
   .then((images) => {
     ReactDOM.render(
       (<div className='image-list'>
-        {images.map((image) => {
+        {shuffle(images).map((image) => {
           return (<div className='image-container' key={image}>
             <img src={`/images/${image}`} />
           </div>)
