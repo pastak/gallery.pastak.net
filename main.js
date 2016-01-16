19920b4b85791b499344b26be78901a8cef271ad
Yamagishi Kazutoshi <ykzts@desire.sh>
2016-01-16 17:14:47 +0900

    React POKU

--- main.js
+++ main.js
@@ -4,17 +4,29 @@ const React = require('react')
 const ReactDOM = require('react-dom')
 const shuffle = require('knuth-shuffle').knuthShuffle
 
-fetch('/images.json')
-  .then((res) => res.json())
-  .then((images) => {
-    ReactDOM.render(
-      (<div className='image-list'>
-        {shuffle(images).map((image) => {
-          return (<div className='image-container' key={image}>
-            <img src={`/images/${image}`} />
-          </div>)
-        })}
-      </div>),
-      document.getElementById('mainView')
-    )
-  })
+const parseJson = res => res.json()
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      images: []
+    }
+  }
+
+  componentDidMount() {
+    fetch('/images.json')
+      .then(parseJson)
+      .then(images => this.setState({images}))
+  }
+
+  render() {
+    return (<div className='image-list'>
+      {shuffle(this.state.images).map(image => (<div className='image-container' key={image}>
+        <img src={`/images/${image}`} />
+      </div>))}
+    </div>)
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('mainView'))
